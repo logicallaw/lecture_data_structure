c6b5069597f49aebfe3874e7dfd513a2c44afdd9
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-06-16 21:26:07 +0900

    Update:week6_p1.cpp/

--- ex1.cpp
+++ ex1.cpp
@@ -1,98 +1,152 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
-struct Node{
+struct node {
     int data;
-    Node* parent;
-    vector<Node*> childList;
-    Node(int data, Node* parent){
+    node* parent;
+    vector<node*> childList;
+    node(int data, node* parent) {
         this->data = data;
         this->parent = parent;
     }
 };
-
-class Tree{
+class Tree {
 private:
-    Node* root;
-    vector<Node*> nodeList;
-    int find(int data, vector<Node*>& List);
+    node* root;
+    vector<node*> nodeList;
+    int find(int data, vector<node*>& List) {
+        for (int i = 0; i < List.size(); i++) {
+            if (List[i]->data == data) {
+                return i;
+            }
+        }
+        return -1;
+    }
 public:
-    Tree(int data);
-    void insertNode(int parData, int data);
-    void deleteNode(int data);
-    void printParent(int data);
-    void printChild(int data);
-};
-
-Tree::Tree(int data){
-    root = new Node(data, NULL);
-    nodeList.push_back(root);
-}
-
-int Tree::find(int data, vector<Node*>& List){
-    for(int i = 0; i < List.size(); i++){
-        if(List[i]->data == data){
-            return i;
+    Tree(int data) {
+        root = new node(data, NULL);
+        nodeList.push_back(root);
+    }
+    void insert(int parData, int data) {
+        if (find(data, nodeList) != -1) {
+            cout << -1 << endl;
+            return;
+        }
+        int idx = find(parData, nodeList);
+        if (idx == -1) {
+            cout << -1 << endl;
+            return;
+        }
+        node* parNode = nodeList[idx];
+        node* newNode = new node(data, parNode);
+        parNode->childList.push_back(newNode);
+        nodeList.push_back(newNode);
+    }
+    void deleteNode(int data) {
+        int idx = find(data, nodeList);
+        if (idx <= 0) {
+            cout << -1 << endl;
+            return;
+        }
+        node* delNode = nodeList[idx];
+        node* parNode = delNode->parent;
+        for (int i = 0; i < delNode->childList.size(); i++) {
+            parNode->childList.push_back(delNode->childList[i]);
+            delNode->childList[i]->parent = parNode;
+        }
+        vector<node*>& child = parNode->childList;
+        child.erase(child.begin() + find(data, child));
+        nodeList.erase(nodeList.begin() + find(data, nodeList));
+        delete delNode;
+    }
+    void min_maxChild(int data) {
+        int idx = find(data, nodeList);
+        if (idx == -1) {
+            cout << -1 << endl;
+            return;
+        }
+        node* curNode = nodeList[idx];
+        vector<node*>& child = curNode->childList;
+        if (child.size() == 0) {
+            cout << -1 << endl;
+            return;
+        }
+        else if (child.size() == 1) {
+            cout << child[0]->data << endl;
+            return;
+        }
+        else {
+            int max = child[0]->data;
+            int min = child[0]->data;
+            for (int i{ 0 }; i < child.size(); i++) {
+                max = (max < child[i]->data) ? child[i]->data : max;
+                min = (min > child[i]->data) ? child[i]->data : min;
+            }
+            cout << max + min << endl;
+            return;
         }
     }
-    return -1;
-}
+    void printParent(int data) {
+        int idx = find(data, nodeList);
+        if (idx <= 0) {
+            cout << "-1" << endl;
+            return;
+        }
+        else {
+            cout << nodeList[idx]->parent->data << endl;
+            return;
+        }
+    }
+    void printChild(int data) {
+        int idx = find(data, nodeList);
+        if (idx == -1) {
+            return;
+        }
+        vector<node*>& child = nodeList[idx]->childList;
+        if (child.size() == 0) {
+            return;
+        }
+        else {
+            for (int i = 0; i < child.size(); i++) {
+                cout << child[i]->data << " ";
+            }
+            cout << endl;
+        }
+    }
+};
 
-void Tree::insertNode(int parData, int data){
-    if(find(data, nodeList) != -1){
-        return;
+int main(void) {
+    Tree tree{ 1 };
+    int size;
+    cin >> size;
+    while (size--) {
+        string userInput;
+        cin >> userInput;
+        if (userInput == "insert") {
+            int x, y;
+            cin >> x >> y;
+            tree.insert(x, y);
+        }
+        else if (userInput == "delete") {
+            int x;
+            cin >> x;
+            tree.deleteNode(x);
+        }
+        else if (userInput == "parent") {
+            int x;
+            cin >> x;
+            tree.printParent(x);
+        }
+        else if (userInput == "child") {
+            int x;
+            cin >> x;
+            tree.printChild(x);
+        }
+        else if (userInput == "min_maxChild") {
+            int x;
+            cin >> x;
+            tree.min_maxChild(x);
+        }
     }
-    int idx = find(parData, nodeList);
-    if(idx == -1){
-        return;
-    }
-    Node* parNode = nodeList[idx];
-    Node* newNode = new Node(data, parNode);
-    parNode->childList.push_back(newNode);
-    nodeList.push_back(newNode);
-}
-
-void Tree::deleteNode(int data){
-    int idx = find(data, nodeList);
-    if(idx == -1){
-        return;
-    }
-    Node* delNode = nodeList[idx];
-    if(delNode == root){
-        return;
-    }
-    Node* parNode = delNode->parent;
-    for(int i = 0; i < delNode->childList.size(); i++){
-        parNode->childList.push_back(delNode->childList[i]);
-        delNode->childList[i]->parent = parNode;
-    }
-
-    vector<Node*>& child = parNode->childList;
-    child.erase(child.begin() + find(data, child));
-    nodeList.erase(nodeList.begin() + idx);
-    delete delNode;
-}
-
-void Tree::printParent(int data){
-    int idx = find(data, nodeList);
-    if(idx <= 0){
-        return;
-    }
-    Node* curNode = nodeList[idx];
-    cout << curNode->parent->data << endl;
-}
-
-void Tree::printChild(int data) {
-    int idx = find(data, nodeList);
-    if(idx == -1){
-        return;
-    }
-    vector<Node*>& child = nodeList[idx]->childList;
-    if(child.empty()){
-        return;
-    }
-    for(int i = 0; i < child.size(); i++){
-        cout << child[i]->data << " ";
-    }
-    cout << endl;
 }
--- week6_p1.cpp
+++ week6_p1.cpp
@@ -2,39 +2,39 @@
 #include <vector>
 #include <string>
 using namespace std;
-struct node{
+struct node {
     int data;
     node* parent;
     vector<node*> childList;
-    node(int data, node* parent){
+    node(int data, node* parent) {
         this->data = data;
         this->parent = parent;
     }
 };
-class Tree{
+class Tree {
 private:
     node* root;
     vector<node*> nodeList;
-    int find(int data, vector<node*>& List){
-        for(int i = 0; i < List.size(); i++){
-            if(List[i]->data == data){
+    int find(int data, vector<node*>& List) {
+        for (int i = 0; i < List.size(); i++) {
+            if (List[i]->data == data) {
                 return i;
             }
         }
         return -1;
     }
 public:
-    Tree(int data){
+    Tree(int data) {
         root = new node(data, NULL);
         nodeList.push_back(root);
     }
-    void insert(int data, int parData){
-        if(find(data, nodeList) != -1){
+    void insert(int parData, int data) {
+        if (find(data, nodeList) != -1) {
             cout << -1 << endl;
             return;
         }
         int idx = find(parData, nodeList);
-        if(idx == -1){
+        if (idx == -1) {
             cout << -1 << endl;
             return;
         }
@@ -43,15 +43,15 @@ public:
         parNode->childList.push_back(newNode);
         nodeList.push_back(newNode);
     }
-    void deleteNode(int data){
+    void deleteNode(int data) {
         int idx = find(data, nodeList);
-        if(idx <= 0){
+        if (idx <= 0) {
             cout << -1 << endl;
             return;
         }
         node* delNode = nodeList[idx];
         node* parNode = delNode->parent;
-        for(int i = 0; i < delNode->childList.size(); i++){
+        for (int i = 0; i < delNode->childList.size(); i++) {
             parNode->childList.push_back(delNode->childList[i]);
             delNode->childList[i]->parent = parNode;
         }
@@ -60,32 +60,36 @@ public:
         nodeList.erase(nodeList.begin() + find(data, nodeList));
         delete delNode;
     }
-    int min_maxChild(int data){
+    void min_maxChild(int data) {
         int idx = find(data, nodeList);
-        if(idx == -1){
-            return -1;
+        if (idx == -1) {
+            cout << -1 << endl;
+            return;
         }
         node* curNode = nodeList[idx];
         vector<node*>& child = curNode->childList;
-        if(child.size() == 0){
-            return 0;
+        if (child.size() == 0) {
+            cout << -1 << endl;
+            return;
         }
-        else if (child.size() == 1){
-            return child[0]->data;
+        else if (child.size() == 1) {
+            cout << child[0]->data << endl;
+            return;
         }
-        else{
+        else {
             int max = child[0]->data;
             int min = child[0]->data;
-            for(int i{ 0 }; i < child.size(); i++){
+            for (int i{ 0 }; i < child.size(); i++) {
                 max = (max < child[i]->data) ? child[i]->data : max;
                 min = (min > child[i]->data) ? child[i]->data : min;
             }
-            return max + min;
+            cout << max + min << endl;
+            return;
         }
     }
-    void printParent(int data){
+    void printParent(int data) {
         int idx = find(data, nodeList);
-        if(idx <= 0){
+        if (idx <= 0) {
             cout << "-1" << endl;
             return;
         }
@@ -94,17 +98,17 @@ public:
             return;
         }
     }
-    void printChild(int data){
+    void printChild(int data) {
         int idx = find(data, nodeList);
-        if(idx == -1){
+        if (idx == -1) {
             return;
         }
         vector<node*>& child = nodeList[idx]->childList;
-        if(child.size() == 0){
+        if (child.size() == 0) {
             return;
         }
         else {
-            for(int i = 0; i < child.size(); i++){
+            for (int i = 0; i < child.size(); i++) {
                 cout << child[i]->data << " ";
             }
             cout << endl;
@@ -112,34 +116,34 @@ public:
     }
 };
 
-int main(void){
+int main(void) {
     Tree tree{ 1 };
     int size;
     cin >> size;
-    while(size > 1){
-        size--;
+    while (size--) {
         string userInput;
-        if(userInput == "insert"){
+        cin >> userInput;
+        if (userInput == "insert") {
             int x, y;
             cin >> x >> y;
-            tree.insert(x,y);
+            tree.insert(x, y);
         }
-        else if (userInput == "delete"){
+        else if (userInput == "delete") {
             int x;
             cin >> x;
             tree.deleteNode(x);
         }
-        else if (userInput == "parent"){
+        else if (userInput == "parent") {
             int x;
             cin >> x;
             tree.printParent(x);
         }
-        else if (userInput == "child"){
+        else if (userInput == "child") {
             int x;
             cin >> x;
             tree.printChild(x);
         }
-        else if (userInput == "min_maxChild"){
+        else if (userInput == "min_maxChild") {
             int x;
             cin >> x;
             tree.min_maxChild(x);
